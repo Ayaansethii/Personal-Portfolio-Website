b670680a745e8301406695e518536597cedc4b4a
Ayaan Sethi <ayaanmsethi@gmail.com>
2024-12-15 14:22:59 -0500

    Updated Navbar: removed Home and Contact buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,12 @@ import { CiFileOn } from "react-icons/ci";
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-4">
+      {/* Logo Section */}
       <div className="flex flex-shrink-0 items-center">
-        <a href="/" aria-label="Home">
-          <img src={logo} alt="logo" className="w-10 h-auto mx-2" />
-        </a>
+        <img src={logo} alt="logo" className="w-10 h-auto mx-2" />
       </div>
+
+      {/* Social Media Links */}
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <a
           href="https://www.linkedin.com/in/ayaansethi/"
